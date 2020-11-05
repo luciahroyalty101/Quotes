a5675086608fde0e1473d21d8c23ae932221ce0b
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-05 16:19:57 +0300

    add array

---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+  quotes: Quote[] = [
+    new Quote(1, 'Seymour Cray','The trouble with programmers is that you can never tell what a programmer is doing until it’s too late.','Lucy Karimi',new Date ),
+    new Quote(2,'Edward V Berard','Walking on water and developing software from a specification are easy if both are frozen. ','Lucy Karimi',new Date ),
+    new Quote(3,'Doug Linder',' A good programmer is someone who always looks both ways before crossing a one-way street.','Lucy Karimi',new Date),
+  ];
+  constructor() { }
+  
+  ngOnInit(): void {
+  }
+
+}
