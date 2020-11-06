3506751793cc5561c262444b9f6cfcb05f0e81ae
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-06 06:38:44 +0300

    edit quotes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,8 +9,8 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(1, 'Seymour Cray','The trouble with programmers is that you can never tell what a programmer is doing until it’s too late.','Lucy Karimi',new Date ),
-    new Quote(2,'Edward V Berard','Walking on water and developing software from a specification are easy if both are frozen. ','Lucy Karimi',new Date ),
-    new Quote(3,'Doug Linder',' A good programmer is someone who always looks both ways before crossing a one-way street.','Lucy Karimi',new Date),
+    new Quote(2,'Edward V Berard','Walking on water and developing software from a specification are easy if both are frozen. ','Ben Deenambo',new Date ),
+    new Quote(3,'Doug Linder',' A good programmer is someone who always looks both ways before crossing a one-way street.','Walter Ruto',new Date),
   ];
   constructor() { }
   
