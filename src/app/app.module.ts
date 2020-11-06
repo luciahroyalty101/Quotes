ddb8a59efb33a515384171c4f05024b5874f443a
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-06 17:38:05 +0300

    edit declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    DateCountPipe,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
