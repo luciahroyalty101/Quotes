47ab7234b1c12a31ccb40b5222d491280ebd919c
luciahroyalty101 <luciahkei101@gmail.com>
2020-11-06 17:51:06 +0300

    add upvote and downvote functionality

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,7 +11,23 @@ export class QuotesComponent implements OnInit {
     new Quote(1, 'Seymour Cray','The trouble with programmers is that you can never tell what a programmer is doing until it’s too late.','Lucy Karimi',new Date ),
     new Quote(2,'Edward V Berard','Walking on water and developing software from a specification are easy if both are frozen. ','Ben Deenambo',new Date ),
     new Quote(3,'Doug Linder',' A good programmer is someone who always looks both ways before crossing a one-way street.','Walter Ruto',new Date),
-  ];
+  ];  
+  upVotes(index){
+    this.quotes[index].upvotes++;
+  }
+  downVotes(index){
+    this.quotes[index].downvotes++; 
+  }
+  deleteQuote(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].state}?`)
+ 
+      if (toDelete){
+        this.quotes.splice(index,1)
+      }
+    }
+  }
+
   constructor() { }
   
   ngOnInit(): void {
